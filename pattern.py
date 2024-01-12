4bb1565ce0cb2ff8bab6f83157217adb17a7fd5e
artnharsh <145191947+artnharsh@users.noreply.github.com>
2024-01-12 19:36:18 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -3,3 +3,13 @@ for i in range(0,x+1,1):
 	for j in range(0,i+1,1):
 		print("* ",end = " ")
 	print("\r")
+//following code for printing numbers
+print("Hello world")
+x=int(input("Enter range:\n"))
+count = 0
+for i in range(1,x+1,1):
+	for j in range(1,i+1,1):
+	    
+		print(j,end = " ")
+		
+	print("\r")
