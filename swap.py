8a5526580cbe782582710cdcaace9d71d99991bd
artnharsh <145191947+artnharsh@users.noreply.github.com>
2024-01-17 13:31:04 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,31 @@
+print("Hello world")
+x=int(input("enter your first number\n"))
+y=int(input("enter your second number\n"))
+x=x^y
+y=x^y
+x=x^y
+print("Swapping of first and second number is:")
+print(x)
+print(y)
+a=int(input("Enter your third number here\n"))
+b=int(input("Enter your fourth number here\n"))
+a,b=b,a
+print("Swapping of third and fourth number is:")
+print(a)
+print(b)
+f=int(input("Enter your fifth number here\n"))
+g=int(input("Enter your sixth number here\n"))
+f=f+g
+g=f-g
+f=f-g
+print("Swapping of fifth and sixth number is:")
+print(f)
+print(g)
+i=int(input("Enter your seventh number here\n"))
+j=int(input("Enter your eighth number here\n"))
+i=i*j
+j=i/j
+i=i/j
+print("Swapping of seventh and eighth number is :")
+print(i)
+print(j)
