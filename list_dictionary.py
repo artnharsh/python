70e225bee7db4b25a1de2910b34df42b92b33ca3
Art_n_Harsh <145191947+artnharsh@users.noreply.github.com>
2024-01-31 14:10:33 +0530

    Add files via upload

---
+++ list_dictionary.py
@@ -0,0 +1,23 @@
+l = []
+
+n = int(input("Enter number of elements : "))
+
+for i in range(0, n):
+	
+	l.append(int(input())) 
+
+print(l)
+
+d = {}
+
+for i in range (0, n):
+
+	if l[i] % 2 == 0 :
+		
+		d[l[i]] = "even"
+	
+	elif l[i]% 2 != 0:
+		
+		d[l[i]] = "odd"
+  
+print(d)
---
+++ string_shift.py
@@ -0,0 +1,3 @@
+text = input("Enter the string :\n")
+new_string = text[8:] + text[:9]
+print("New string is :\n", new_string)
